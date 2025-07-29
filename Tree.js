4db8d31dbad30674e9b5b6e6cc62a310d4809052
cdogcoder <cdogcoder@gmail.com>
2025-07-29 12:09:59 -0700

    Temporarily implement print function for BST

--- Tree.js
+++ Tree.js
@@ -15,8 +15,24 @@ export default class Tree {
             return newNode
         }
     }
+
 }
+
+function prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+};
 
 const t = new Tree();
 const arr = [1,2,3,4,5,6,7]
-console.log(t.buildTree(arr, 0, arr.length-1))
+t.root = t.buildTree(arr, 0, arr.length-1);
+
+prettyPrint(t.root)
